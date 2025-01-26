77dcda55ab54fa646ea60818af6ff7f0b32176d8
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-26 21:38:29 +0300

    refactor: update SEO constants to use environment variables for site title and description

--- src/constants/seo.constants.ts
+++ src/constants/seo.constants.ts
@@ -1,2 +1,4 @@
-export const NO_INDEX_PAGE = {robots: {index: false, follow: false}}
-export const SITE_NAME = 'Table planer'
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+export const SITE_NAME = process.env['NEXT_PUBLIC_TITLE'] || 'Price Calc'
+export const SITE_DESCRIPTION =
+    process.env['NEXT_PUBLIC_DESCRIPTION'] || 'Free price calculator'
