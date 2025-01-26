a40d23eca0920f068a513b3600613b0a4d067d1a
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-26 16:47:55 +0300

    remove type

--- src/store/hooks/use-app-dispatch.ts
+++ src/store/hooks/use-app-dispatch.ts
@@ -1,4 +1,4 @@
-import { UseDispatch, useDispatch } from 'react-redux'
+import { type UseDispatch, useDispatch } from 'react-redux'
 import type { AppDispatch } from '@/store/store'
 
 export const useAppDispatch: UseDispatch<AppDispatch> =
