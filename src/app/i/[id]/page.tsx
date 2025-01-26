b3ea61e88619fc162db8c5960d450876cb19d4e5
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-26 22:18:27 +0300

    refactor: remove unused generateStaticParams function from page component

--- src/app/i/[id]/page.tsx
+++ src/app/i/[id]/page.tsx
@@ -2,12 +2,6 @@ import { use } from 'react'
 import { ItemId } from '@/components/pages/item/ItemId'
 import type { ItemIdPageProps } from '@/shared/interfaces/item.interface'
 
-export async function generateStaticParams() {
-    const products = Array.from({ length: 10 }, (_, i) => ({ id: i }))
-
-    return products.map(product => ({ id: product.id.toString() }))
-}
-
 export default function Page({ params }: ItemIdPageProps) {
     const { id } = use(params)
 
