fc18b7c0c02fa6320fc93c75ba6fdcffdcd54fa7
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-26 19:57:24 +0300

    refactor: remove outdated internationalization configuration and update plugin initialization

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const withNextIntl = createNextIntlPlugin('@/src/i18n/request.ts')
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
     /* config options here */
+    output: 'export'
 }
 
 export default withNextIntl(nextConfig)
