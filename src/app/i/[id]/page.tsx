cabfdf7030f762148f05a4aaccfa512fcacd77f2
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-26 17:57:06 +0300

    refactor: remove unused getStaticProps and clean up imports in page component

--- src/app/i/[id]/page.tsx
+++ src/app/i/[id]/page.tsx
@@ -1,37 +1,11 @@
 import { use } from 'react'
 import { ItemId } from '@/components/pages/item/ItemId'
 import type { ItemIdPageProps } from '@/shared/interfaces/item.interface'
-import { defaultCurrency } from '@/store/currency/currency'
-import type { IProduct } from '@/store/products/products.types'
-import { defaultUnit } from '@/store/unit/unit'
 
 export async function generateStaticParams() {
     const products = Array.from({ length: 10 }, (_, i) => ({ id: i }))
 
     return products.map(product => ({ id: product.id.toString() }))
-}
-
-export async function getStaticProps() {
-    // context: GetStaticPropsContext
-    const data: IProduct = {
-        name: 'Product name',
-        rounded: 0,
-        costRounded: 0,
-        cost: 0,
-        quantity: 0,
-        unit: defaultUnit,
-        currency: defaultCurrency
-    }
-
-    if (!data) {
-        return {
-            notFound: true
-        }
-    }
-
-    return {
-        props: { data }
-    }
 }
 
 export default function Page({ params }: ItemIdPageProps) {
