8d8bd82d800266e0219114ec86bd3041eafc6e5b
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-26 17:33:22 +0300

    fix: remove output option from next.config.ts and simplify Page component

--- src/app/i/[id]/page.tsx
+++ src/app/i/[id]/page.tsx
@@ -1,15 +1,9 @@
+import { use } from 'react'
 import { ItemId } from '@/components/pages/item/ItemId'
 import type { ItemIdPageProps } from '@/shared/interfaces/item.interface'
-import { getProducts } from '@/store/products/products'
 
-export async function generateStaticParams() {
-    const products = await getProducts()
-
-    return products.map((_, id) => ({ id }))
-}
-
-export default async function Page({ params }: ItemIdPageProps) {
-    const { id } = await params
+export default function Page({ params }: ItemIdPageProps) {
+    const { id } = use(params)
 
     return <ItemId id={id} />
 }
