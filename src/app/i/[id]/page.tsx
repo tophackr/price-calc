032358b2827b1eeb8e07b89b6eef62186348a148
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-26 17:18:04 +0300

    feat: add generateStaticParams function to fetch product IDs for static generation

--- src/app/i/[id]/page.tsx
+++ src/app/i/[id]/page.tsx
@@ -1,8 +1,15 @@
 import { ItemId } from '@/components/pages/item/ItemId'
 import type { ItemIdPageProps } from '@/shared/interfaces/item.interface'
+import { getProducts } from '@/store/products/products'
 
 export default async function Page({ params }: ItemIdPageProps) {
     const { id } = await params
 
     return <ItemId id={id} />
+}
+
+export async function generateStaticParams() {
+    const products = await getProducts()
+
+    return products.map((_, id) => ({ id }))
 }
