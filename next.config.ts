b3efe7d9bdb3dd1abb1c02e4a436468d14a20508
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-27 11:20:47 +0300

    refactor: enable React strict mode and disable SWC minification in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin()
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    swcMinify: false
+}
 
 export default withNextIntl(nextConfig)
