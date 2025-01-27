eaae04058455c4088b432e29105e1c64f2d7e1fb
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-27 11:51:06 +0300

    refactor: convert Page component to async and await params destructuring

--- src/app/i/[id]/page.tsx
+++ src/app/i/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { use } from 'react'
 import { ItemId } from '@/components/pages/item/ItemId'
 import type { ItemIdPageProps } from '@/shared/interfaces/item.interface'
 
-export default function Page({ params }: ItemIdPageProps) {
-    const { id } = use(params)
+export default async function Page({ params }: ItemIdPageProps) {
+    const { id } = await params
 
     return <ItemId id={id} />
 }
