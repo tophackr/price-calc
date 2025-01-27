6474a00ba848563e733e151277d4458b8e985512
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-27 12:49:46 +0300

    refactor: remove RootLayout from HomeLayout and simplify header rendering

--- src/components/layout/home/Home.layout.tsx
+++ src/components/layout/home/Home.layout.tsx
@@ -3,13 +3,14 @@
 import { isCloudStorageSupported } from '@telegram-apps/sdk-react'
 import { List } from '@telegram-apps/telegram-ui'
 import type { PropsWithChildren } from 'react'
-import { RootLayout } from '../app/Root.layout'
 import { Header } from './header/Header'
 
 export function HomeLayout({ children }: PropsWithChildren) {
     return (
-        <RootLayout header={isCloudStorageSupported() && <Header />}>
+        <>
+            {isCloudStorageSupported() && <Header />}
+
             <List>{children}</List>
-        </RootLayout>
+        </>
     )
 }
