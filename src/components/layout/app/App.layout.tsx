3d6b113f337f8f6bbb8bcdd363a371f832ae12b7
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-27 11:23:48 +0300

    refactor: wrap AppLayout in React StrictMode for improved performance

--- src/components/layout/app/App.layout.tsx
+++ src/components/layout/app/App.layout.tsx
@@ -1,18 +1,20 @@
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { useLocale } from 'next-intl'
-import type { PropsWithChildren } from 'react'
+import { type PropsWithChildren, StrictMode } from 'react'
 import { Providers } from '@/app/providers'
 
 export function AppLayout({ children }: PropsWithChildren) {
     const locale = useLocale()
 
     return (
-        <html lang={locale}>
-            <body>
-                <Providers>{children}</Providers>
+        <StrictMode>
+            <html lang={locale}>
+                <body>
+                    <Providers>{children}</Providers>
 
-                <SpeedInsights />
-            </body>
-        </html>
+                    <SpeedInsights />
+                </body>
+            </html>
+        </StrictMode>
     )
 }
