da3bd751224f299b34a80e431c29e3abeb844f31
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-27 12:49:51 +0300

    refactor: remove RootLayout from PageLayout and simplify rendering

--- src/components/layout/page/Page.layout.tsx
+++ src/components/layout/page/Page.layout.tsx
@@ -1,11 +1,6 @@
 import { List } from '@telegram-apps/telegram-ui'
 import type { PropsWithChildren } from 'react'
-import { RootLayout } from '../app/Root.layout'
 
 export default function PageLayout({ children }: PropsWithChildren) {
-    return (
-        <RootLayout>
-            <List>{children}</List>
-        </RootLayout>
-    )
+    return <List>{children}</List>
 }
