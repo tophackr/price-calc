69cc31c4ce388bc43028eda7a32df57262238343
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-01-27 15:06:34 +0300

    refactor: update IProduct type to use ItemOrder instead of ItemForm

--- src/store/products/products.types.ts
+++ src/store/products/products.types.ts
@@ -1,5 +1,5 @@
 import type { ICalculateCosts } from '@/shared/interfaces/calculate-cost.interface'
-import type { ItemForm } from '@/shared/interfaces/item.interface'
+import type { ItemOrder } from '@/shared/interfaces/item.interface'
 import type { Measurement } from '@/shared/interfaces/measurement.interface'
 
-export type IProduct = ICalculateCosts & ItemForm & Measurement
+export type IProduct = ICalculateCosts & ItemOrder & Measurement
