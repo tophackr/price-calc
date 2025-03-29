d87a245838bc25acc2ec6843f70e2f4cc5123934
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-03-29 15:13:49 +0300

    fix: ensure miniApp is ready during initialization

--- src/core/init.ts
+++ src/core/init.ts
@@ -48,6 +48,8 @@ export function init(debug: boolean): void {
     if (!miniApp.isCssVarsBound()) miniApp.bindCssVars()
     if (!themeParams.isCssVarsBound()) themeParams.bindCssVars()
 
+    miniApp.ready()
+
     // Add Eruda if needed.
     if (debug) {
         import('eruda').then(lib => lib.default.init()).catch(console.error)
