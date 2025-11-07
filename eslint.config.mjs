e4a3e9be90a122afbf71222550159b4f5e50403c
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-11-07 20:13:10 +0300

    build: update next version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { FlatCompat } from '@eslint/eslintrc'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -9,13 +11,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname
 })
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-  {
-    rules: {
-      'react/display-name': 'off'
-    }
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.extends("prettier"), {
+  rules: {
+    'react/display-name': 'off'
   }
-]
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}]
 
 export default eslintConfig
